3046ba16cc640d75807add58320eb78f0dbac2bb
Krmueller1s <35940724+Krmueller1s@users.noreply.github.com>
2018-01-29 18:50:15 -0600

    add function add
    this function will return the sum of two numbers.

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+int add(int x, int y);
 int main()
 {
 std::cout << "Hello World\n";
 std::cout << "a= " << a << '\n';
 std::cout << "Added in" << endl;
 return 0;
+}
+int add(int x, int y)
+{
+  return x+ y;
 }
